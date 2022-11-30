c0bf7338e7c2c4e22600f6afb2d553717c8fed5c
Aditya <shadowmonarch712@gmail.com>
2022-11-30 23:13:58 +0530

    part 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,24 @@
-import React, {useState, useEffect} from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
-import {useDispatch} from 'react-redux';
-import getRequests from './actions/requests';
+import React from "react";
+import { Container } from "@mui/material";
+import Navbar from "./components/Navbar/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
-import Requests from './components/Requests/Requests.js';
-import Form from './components/Form/Form.js';
-import useStyles from './styles';
-
-const App = () => {
-   const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(()=>{
-    dispatch(getRequests());
-  },[currentId,dispatch]); 
-
-  return (
+const App = () => (
+  <Router>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          KHOJ
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Requests setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+      </Routes>
     </Container>
-  );
-};
+  </Router>
+);
 
 export default App;
